c9eded98b1923f918563c265c1c6eab2668653ca
six-standard <dbrrl1127@gmail.com>
2024-11-24 02:23:30 +0900

    feature: 컬러셋 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,29 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
+    colors: {
+      bg: {
+        1: '#121212',
+        2: '#1E1E1E',
+        3: '#252525',
+      },
+      text: {
+        1: '#ECECEC',
+        2: '#D9D9D9',
+        3: '#ACACAC',
+      },
+      border: {
+        1: '#2A2A2A',
+        2: '#4D4D4D',
+        3: '#E0E0E0',
+      },
+      primary: {
+        1: '#96F2D7',
+        2: '#63E6BE',
       },
     },
   },
   plugins: [],
 };
+
 export default config;
