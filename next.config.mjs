a46e329b2f5c44b00df63bc1186047b60e0c18d1
six-standard <dbrrl1127@gmail.com>
2024-11-26 09:55:07 +0900

    feature: Proxy 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/graphql',
+        destination: 'https://v3.velog.io/graphql',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
--- src/app/(login)/page.tsx
+++ src/app/(login)/page.tsx
@@ -2,10 +2,62 @@
 
 import { Button, Input } from '@/components';
 import { useForm } from 'react-hook-form';
+import cookie from 'js-cookie';
 
 interface formVo {
   access_token: string;
   refresh_token: string;
+}
+
+async function fetchCurrentUser() {
+  const url = '/graphql';
+  const headers = {
+    'content-type': 'application/json',
+    cookie:
+      'access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiOWZkMjdhZTQtZTZkMi00OWQ3LWJjMDEtNGZmOTg2YmRkOGIyIiwiaWF0IjoxNzMyNDk2NDY1LCJleHAiOjE3MzI1ODI4NjUsImlzcyI6InZlbG9nLmlvIiwic3ViIjoiYWNjZXNzX3Rva2VuIn0.lATs_JP1jVAubwra_ZUobvkprk_Yph_EtOA0t7UpBCg;',
+  };
+  const body = JSON.stringify({
+    query: `
+      query currentUser {
+        currentUser {
+          id
+          username
+          email
+          profile {
+            id
+            thumbnail
+            display_name
+            short_bio
+            profile_links
+          }
+          user_meta {
+            id
+            email_notification
+            email_promotion
+          }
+        }
+      }
+    `,
+    variables: {},
+  });
+
+  fetch(url, {
+    method: 'POST',
+    headers: headers,
+    body: body,
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error: Status ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
 }
 
 export default function Page() {
@@ -16,7 +68,9 @@ export default function Page() {
   } = useForm<formVo>({ mode: 'onChange' });
 
   const onSubmit = (data: formVo) => {
-    console.log(data);
+    cookie.set('access_token', data.access_token);
+    cookie.set('refresh_token', data.refresh_token);
+    fetchCurrentUser();
   };
 
   return (
