25d9239e39b63029338288ac7776103ab854b153
six-standard <dbrrl1127@gmail.com>
2024-11-28 16:48:33 +0900

    modify: queryClient 옵션 추가

--- src/utils/QueryProvider.tsx
+++ src/utils/QueryProvider.tsx
@@ -3,7 +3,14 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactNode } from 'react';
 
-const client = new QueryClient();
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 interface IProp {
   children: React.ReactElement | React.ReactElement[] | ReactNode | ReactNode[];
