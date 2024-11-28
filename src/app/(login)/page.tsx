2b7af66433c8a019141af487617749489f2c3fd3
six-standard <dbrrl1127@gmail.com>
2024-11-28 15:34:05 +0900

    feature: 로그인 페이지 제작

--- src/app/(login)/page.tsx
+++ src/app/(login)/page.tsx
@@ -3,74 +3,37 @@
 import { Button, Input } from '@/components';
 import { useForm } from 'react-hook-form';
 import cookie from 'js-cookie';
+import { useMutation } from '@tanstack/react-query';
+import { instance } from '../../api';
+import { useRouter } from 'next/navigation';
+import { toast } from 'react-toastify';
 
 interface formVo {
   access_token: string;
   refresh_token: string;
 }
 
-async function fetchCurrentUser() {
-  const url = '/graphql';
-  const headers = {
-    'content-type': 'application/json',
-    cookie:
-      'access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiOWZkMjdhZTQtZTZkMi00OWQ3LWJjMDEtNGZmOTg2YmRkOGIyIiwiaWF0IjoxNzMyNDk2NDY1LCJleHAiOjE3MzI1ODI4NjUsImlzcyI6InZlbG9nLmlvIiwic3ViIjoiYWNjZXNzX3Rva2VuIn0.lATs_JP1jVAubwra_ZUobvkprk_Yph_EtOA0t7UpBCg;',
-  };
-  const body = JSON.stringify({
-    query: `
-      query currentUser {
-        currentUser {
-          id
-          username
-          email
-          profile {
-            id
-            thumbnail
-            display_name
-            short_bio
-            profile_links
-          }
-          user_meta {
-            id
-            email_notification
-            email_promotion
-          }
-        }
-      }
-    `,
-    variables: {},
-  });
-
-  fetch(url, {
-    method: 'POST',
-    headers: headers,
-    body: body,
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error: Status ${response.status}`);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-}
-
 export default function Page() {
+  const { replace } = useRouter();
+
   const {
     register,
     handleSubmit,
     formState: { isValid },
   } = useForm<formVo>({ mode: 'onChange' });
 
+  const { mutate } = useMutation({
+    mutationFn: async () => await instance('/login', { method: 'POST' }),
+    onSuccess: () => replace('/main'),
+    onError: (res: Response) => {
+      toast.error(`${res.statusText} (${res.status})`);
+    },
+  });
+
   const onSubmit = (data: formVo) => {
     cookie.set('access_token', data.access_token);
     cookie.set('refresh_token', data.refresh_token);
-    fetchCurrentUser();
+    mutate();
   };
 
   return (
