9da1f6c8f71a19987b51846b310e10f7677e99c2
six-standard <dbrrl1127@gmail.com>
2024-11-28 16:30:24 +0900

    modify: BASE_URL 안정성 강화

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,13 @@
 import returnFetch from 'return-fetch';
 
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+if (!BASE_URL) {
+  throw new Error('BASE_URL가 ENV에서 설정되지 않았습니다.');
+}
+
 export const instance = returnFetch({
-  baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
+  baseUrl: BASE_URL,
   headers: { Accept: 'application/json' },
   interceptors: {
     response: async (response) => {
