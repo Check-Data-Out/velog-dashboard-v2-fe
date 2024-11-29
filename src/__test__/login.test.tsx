97cba3fa33c43848df84b441b1b0f3bc9f33071c
six-standard <dbrrl1127@gmail.com>
2024-11-29 19:01:08 +0900

    refactor: 반복된 코드 정리

--- src/__test__/login.test.tsx
+++ src/__test__/login.test.tsx
@@ -7,8 +7,29 @@ import { ToastContainer } from 'react-toastify';
 import { useRouter } from 'next/navigation';
 
 jest.mock('next/navigation', () => ({
-  useRouter: jest.fn(), // useRouter를 jest.fn()으로 설정
+  useRouter: jest.fn(),
 }));
+
+const getElements = () => {
+  const buttonEl = screen.getByRole('button');
+  const accessInputEl = screen.getByPlaceholderText(
+    'Access Token을 입력하세요',
+  );
+  const refreshInputEl = screen.getByPlaceholderText(
+    'Refresh Token을 입력하세요',
+  );
+
+  return { buttonEl, accessInputEl, refreshInputEl };
+};
+
+const renderPage = () => {
+  renderWithQueryClient(
+    <>
+      <ToastContainer autoClose={2000} />
+      <Page />
+    </>,
+  );
+};
 
 describe('로그인 화면에서', () => {
   beforeEach(() => {
@@ -16,12 +37,9 @@ describe('로그인 화면에서', () => {
   });
 
   it('입력 칸이 하나라도 비어있으면 버튼이 비활성화된다.', async () => {
-    renderWithQueryClient(<Page />);
+    renderPage();
 
-    const buttonEl = screen.getByRole('button');
-    const accessInputEl = screen.getByPlaceholderText(
-      'Access Token을 입력하세요',
-    );
+    const { buttonEl, accessInputEl } = getElements();
 
     await userEvent.type(accessInputEl, 'access');
 
@@ -29,55 +47,51 @@ describe('로그인 화면에서', () => {
   });
 
   it('액세스 토큰과 리프레시 토큰을 입력하면 버튼이 활성화된다', async () => {
-    renderWithQueryClient(<Page />);
+    renderPage();
 
-    const buttonEl = screen.getByRole('button');
-    const accessInputEl = screen.getByPlaceholderText(
-      'Access Token을 입력하세요',
-    );
-    const refrehsInputEl = screen.getByPlaceholderText(
-      'Refresh Token을 입력하세요',
-    );
+    const { buttonEl, accessInputEl, refreshInputEl } = getElements();
 
     await userEvent.type(accessInputEl, 'access');
-    await userEvent.type(refrehsInputEl, 'refresh');
+    await userEvent.type(refreshInputEl, 'refresh');
 
     expect(buttonEl).toBeEnabled();
   });
 
   describe('API 요청에서', () => {
-    it('액세스 토큰이 비정상적이면 오류 토스트가 표기된다', async () => {
-      fetchMock.mockRejectOnce(new Error());
+    it('서버가 응답하지 않으면 오류 토스트가 표기된다', async () => {
+      renderPage();
 
-      renderWithQueryClient(
-        <>
-          <ToastContainer autoClose={2000} />
-          <Page />
-        </>,
-      );
+      fetchMock.mockAbortOnce();
 
-      const buttonEl = screen.getByRole('button');
-      const accessInputEl = screen.getByPlaceholderText(
-        'Access Token을 입력하세요',
-      );
-      const refrehsInputEl = screen.getByPlaceholderText(
-        'Refresh Token을 입력하세요',
-      );
+      const { buttonEl, accessInputEl, refreshInputEl } = getElements();
 
       await userEvent.type(accessInputEl, 'invalid_access');
-      await userEvent.type(refrehsInputEl, 'invalid_refresh');
+      await userEvent.type(refreshInputEl, 'invalid_refresh');
+      await act(async () => buttonEl.click());
 
-      await act(async () => {
-        // tanstack-query가 동작되는 게 state 변경이라 act가 없으면 오류 발생
-        // (아마 act가 브라우저 환경을 모방하지 않을까 추측중)
-        buttonEl.click();
-      });
+      screen.debug();
+      const toastEl = screen.getByText('유효하지 않은 토큰 (404)');
+      expect(toastEl).toBeInTheDocument();
+    });
 
-      const toastEl = screen.getByText('유효하지 않은 토큰 (404)'); // 이 메세지여야만 한다. (근데 Velog에서 반한하는 메세지가 뭔지 모르겠다)
+    it('액세스 토큰이 비정상적이면 오류 토스트가 표기된다', async () => {
+      renderPage();
+
+      fetchMock.mockRejectOnce(new Error());
+
+      const { buttonEl, accessInputEl, refreshInputEl } = getElements();
+
+      await userEvent.type(accessInputEl, 'invalid_access');
+      await userEvent.type(refreshInputEl, 'invalid_refresh');
+      await act(async () => buttonEl.click());
+
+      const toastEl = screen.getByText('유효하지 않은 토큰 (404)');
       expect(toastEl).toBeInTheDocument();
     });
 
     it('액세스 토큰이 정상적이면 페이지를 대시보드로 이동시킨다', async () => {
+      renderPage();
+
       const replace = jest.fn();
       (useRouter as jest.Mock).mockImplementation(() => ({ replace }));
 
@@ -87,18 +101,10 @@ describe('로그인 화면에서', () => {
         json: () => ({}),
       } as Response);
 
-      renderWithQueryClient(<Page />);
-
-      const buttonEl = screen.getByRole('button');
-      const accessInputEl = screen.getByPlaceholderText(
-        'Access Token을 입력하세요',
-      );
-      const refrehsInputEl = screen.getByPlaceholderText(
-        'Refresh Token을 입력하세요',
-      );
+      const { buttonEl, accessInputEl, refreshInputEl } = getElements();
 
       await userEvent.type(accessInputEl, 'access');
-      await userEvent.type(refrehsInputEl, 'refresh');
+      await userEvent.type(refreshInputEl, 'refresh');
       await act(async () => buttonEl.click());
 
       expect(replace).toHaveBeenCalledWith('/main');
