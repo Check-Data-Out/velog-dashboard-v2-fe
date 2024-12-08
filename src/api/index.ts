9f8f918c7a6e09ea7e09d90ac611a5d2d6f157a2
six-standard <dbrrl1127@gmail.com>
2024-12-09 00:23:49 +0900

    refactor: 오류 메세지 수정

--- src/api/index.ts
+++ src/api/index.ts
@@ -56,7 +56,9 @@ export const instance = async (
       throw new ServerNotRespondingError();
     else {
       if (!error || !(error && error[`${(err as Response).status}`]))
-        throw new Error(`서버에서 Z 오류가 발생했습니다. (${err.name})`);
+        throw new Error(
+          `서버에서 예기치 않은 오류가 발생했습니다. (${err.name})`,
+        );
       throw error[`${(err as Response).status}`];
     }
   }
