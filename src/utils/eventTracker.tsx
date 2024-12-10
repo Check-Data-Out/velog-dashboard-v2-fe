3ce90e9b27e9f74492ebb82fb7915a29d6559382
six-standard <dbrrl1127@gmail.com>
2024-12-10 09:13:06 +0900

    feature: 이벤트 로거 생성 (유저 이벤트, 방문 이벤트)

---
+++ src/utils/eventTracker.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import { instance } from '@/api';
+import { useEffect, useRef } from 'react';
+import { usePathname } from 'next/navigation';
+
+type VisitDataType = {
+  loadDate: null | string;
+  unloadDate: null | string;
+  visitTime: null | string;
+  path: string;
+};
+
+export const trackUserEvent = (message: string) => {
+  const { pathname } = window.location;
+  instance('/track/user', { body: { path: pathname, message } });
+};
+
+export const TrackVisitEvent = () => {
+  const pathname = usePathname();
+
+  const data = useRef<VisitDataType>({
+    loadDate: null,
+    unloadDate: null,
+    visitTime: null,
+    path: pathname,
+  });
+
+  const setUnloadData = () => {
+    const date = new Date().toISOString();
+    data.current = {
+      ...data.current,
+      unloadDate: date,
+      visitTime: (
+        new Date(date).getTime() -
+        new Date(data.current.loadDate as string).getTime()
+      ).toString(),
+    };
+    navigator.sendBeacon(`${process.env.NEXT_PUBLIC_BASE_URL}/track/visit`);
+  };
+
+  useEffect(() => {
+    // 페이지 로드 시 시간 기록
+    data.current.loadDate = new Date().toISOString();
+    window.addEventListener('unload', setUnloadData);
+
+    return () => {
+      window.removeEventListener('unload', setUnloadData);
+    };
+  }, []);
+
+  return <span className="hidden">eventTracker</span>;
+};
