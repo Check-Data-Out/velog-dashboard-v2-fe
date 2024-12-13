770b0808f5f329de9b45b9c412a5990444b6fec7
six-standard <dbrrl1127@gmail.com>
2024-12-13 16:57:38 +0900

    feature: 로그인 페이지 디자인 변경

--- src/app/(login)/Content.tsx
+++ src/app/(login)/Content.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-import { useMutation } from '@tanstack/react-query';
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
+
+import { instance } from '@/api';
 import { Button, Input } from '@/components';
 import { NotFoundError } from '@/errors';
-import { instance } from '@/api';
+import { useMutation } from '@tanstack/react-query';
 
 interface FormVo {
   access_token: string;
@@ -44,34 +46,39 @@ export const Content = () => {
     <main className="w-full h-full flex items-center justify-center">
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="w-fit h-fit flex flex-col gap-[30px] items-center p-[30px] bg-bg-sub rounded-[4px] shadow-[0_4px_16px_0_rgba(0,0,0,.04)]"
+        className="w-fit h-[480px] flex overflow-hidden bg-bg-sub rounded-[4px] shadow-[0_4px_16px_0_rgba(0,0,0,.04)]"
       >
-        <h1 className="font-medium text-[32px] text-text-main">
-          Velog Dashboard
-        </h1>
-        <Input
-          id="access"
-          size="LARGE"
-          type="password"
-          placeholder="Access Token을 입력하세요"
-          {...register('access_token', { required: true })}
-        />
-        <Input
-          id="refresh"
-          size="LARGE"
-          type="password"
-          placeholder="Refresh Token을 입력하세요"
-          {...register('refresh_token', { required: true })}
-        />
-        <Button
-          size="LARGE"
-          form="LARGE"
-          type="submit"
-          disabled={!isValid}
-          id="login"
-        >
-          로그인
-        </Button>
+        <div className="w-[220px] h-full bg-bg-alt flex items-center justify-center">
+          <Image width={96} height={96} src="/favicon.png" alt="로고 이미지" />
+        </div>
+        <div className="w-[500px] h-full flex flex-col gap-[30px] items-center justify-center">
+          <h1 className="font-medium text-[32px] text-text-main">
+            Velog Dashboard
+          </h1>
+          <Input
+            id="access"
+            size="LARGE"
+            type="password"
+            placeholder="Access Token을 입력하세요"
+            {...register('access_token', { required: true })}
+          />
+          <Input
+            id="refresh"
+            size="LARGE"
+            type="password"
+            placeholder="Refresh Token을 입력하세요"
+            {...register('refresh_token', { required: true })}
+          />
+          <Button
+            size="LARGE"
+            form="LARGE"
+            type="submit"
+            disabled={!isValid}
+            id="login"
+          >
+            로그인
+          </Button>
+        </div>
       </form>
     </main>
   );
