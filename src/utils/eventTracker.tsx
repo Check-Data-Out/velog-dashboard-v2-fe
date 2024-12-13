22c47e5c1f712997835b949bc70387643ebf9c09
six-standard <dbrrl1127@gmail.com>
2024-12-13 16:54:46 +0900

    refactor: path 변경

--- src/utils/eventTracker.tsx
+++ src/utils/eventTracker.tsx
@@ -12,10 +12,12 @@ type VisitDataType = {
   path: string;
 };
 
-export const trackUserEvent = (message: string) => {
+type messages = '01' | '02' | '03' | '04' | '99';
+
+export const trackUserEvent = (event_type: messages) => {
   const { pathname } = window.location;
-  instance('/track/user', {
-    body: { path: pathname, message },
+  instance('/event', {
+    body: { path: pathname, event_type },
     method: 'POST',
   });
 };
@@ -34,14 +36,14 @@ export const TrackVisitEvent = () => {
     const date = new Date().toISOString();
     data.current = {
       ...data.current,
-      unloadDate: date,
-      visitTime: (
+      visitTime: BigInt(
         new Date(date).getTime() -
-        new Date(data.current.loadDate as string).getTime()
+          new Date(data.current.loadDate as string).getTime(),
       ).toString(),
+      unloadDate: date,
     };
     navigator.sendBeacon(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/track/visit`,
+      `${process.env.NEXT_PUBLIC_BASE_URL}/stay`,
       data.current.toString(),
     );
   };
