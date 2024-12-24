fd7abc5dc2764ed3f104fed88861f8f903f4f092
six-standard <dbrrl1127@gmail.com>
2024-12-24 16:12:44 +0900

    refactor: 네이밍 규칙 맞춤

--- src/components/auth-required/main/Summary/index.tsx
+++ src/components/auth-required/main/Summary/index.tsx
@@ -6,20 +6,12 @@ import { SidebarContent } from './SidebarContent';
 import { BarContent } from './BarContent';
 
 interface IProp {
-  total_views: number;
   views: number;
-  total_likes: number;
   likes: number;
-  total_posts: number;
+  posts: number;
 }
 
-export const Summary = ({
-  total_views,
-  total_likes,
-  total_posts,
-  views,
-  likes,
-}: IProp) => {
+export const Summary = ({ posts, views, likes }: IProp) => {
   const [open, setOpen] = useState(false);
 
   return (
@@ -27,16 +19,16 @@ export const Summary = ({
       <aside className="flex flex-col h-full gap-[30px] max-TBL:gap-[20px] max-MBI:hidden">
         <SidebarContent
           title="전체 조회수"
-          content={total_views}
+          content={views}
           prefix="회"
           increasement={views}
         />
         <SidebarContent
           title="전체 좋아요 수"
-          content={total_likes}
+          content={likes}
           increasement={likes}
         />
-        <SidebarContent title="총 게시글 수" content={total_posts} />
+        <SidebarContent title="총 게시글 수" content={posts} />
       </aside>
       <section
         className={`MBI:hidden flex flex-col w-full bg-bg-sub rounded-[4px] cursor-pointer`}
@@ -50,16 +42,16 @@ export const Summary = ({
           <div className="w-full h-fit flex flex-col py-[20px] px-[35px] shrink-0 gap-[10px]">
             <BarContent
               title="전체 조회수"
-              content={total_views}
+              content={views}
               prefix="회"
               increasement={views}
             />
             <BarContent
               title="전체 좋아요 수"
-              content={total_likes}
+              content={likes}
               increasement={likes}
             />
-            <BarContent title="총 게시글 수" content={total_posts} />
+            <BarContent title="총 게시글 수" content={posts} />
           </div>
         )}
       </section>
