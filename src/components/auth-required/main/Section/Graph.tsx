81f24c65fbf1c3dde6ef64882efe1611617967f8
six-standard <dbrrl1127@gmail.com>
2024-12-24 16:08:54 +0900

    feature: 그래프 추가

---
+++ src/components/auth-required/main/Section/Graph.tsx
@@ -0,0 +1,104 @@
+'use client';
+
+import { Line } from 'react-chartjs-2';
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { COLORS, SCREENS } from '@/constants';
+import { Button, Dropdown, Input } from '@/components';
+import { useResponsive } from '@/hooks';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+);
+
+const data = {
+  labels: [
+    '2024-12-11',
+    '2024-12-12',
+    '2024-12-13',
+    '2024-12-14',
+    '2024-12-15',
+    '2024-12-16',
+  ],
+  datasets: [
+    {
+      label: 'Views',
+      data: [1032, 941, 513, 752, 469, 310],
+      fill: true,
+      backgroundColor: COLORS.text.main,
+      borderColor: COLORS.primary.main,
+    },
+  ],
+};
+
+export const Graph = () => {
+  const width = useResponsive();
+  const isMBI = width < SCREENS.MBI;
+
+  return (
+    <div className="w-full bg-bg-sub pb-7 max-MBI:pb-14 flex flex-col items-center px-8 gap-[30px]">
+      <div className="flex items-center gap-5 max-MBI:gap-[10px] max-MBI:flex-col">
+        <div className="flex items-center gap-[inherit]">
+          <Input
+            size={isMBI ? 'SMALL' : 'MEDIUM'}
+            form="SMALL"
+            placeholder="시작 날짜"
+            type="date"
+          />
+          <span className="font-bold text-[20px] text-text-main">~</span>
+          <Input
+            size={isMBI ? 'SMALL' : 'MEDIUM'}
+            form="SMALL"
+            placeholder="종료 날짜"
+            type="date"
+          />
+          <Dropdown options={['조회수', '좋아요', '댓글']} />
+        </div>
+
+        <Button size="SMALL">조회</Button>
+      </div>
+      <Line
+        data={data}
+        options={{
+          responsive: true,
+          animation: false,
+          plugins: {
+            legend: {
+              display: false,
+            },
+          },
+          scales: {
+            x: {
+              axis: 'x',
+              grid: {
+                color: COLORS.border.sub,
+              },
+            },
+            y: {
+              axis: 'y',
+              grid: {
+                color: COLORS.border.sub,
+              },
+            },
+          },
+        }}
+        className="w-[100%_!important] h-[auto_!important] max-h-[300px]"
+      />
+    </div>
+  );
+};
--- src/components/auth-required/main/Section/index.tsx
+++ src/components/auth-required/main/Section/index.tsx
@@ -3,25 +3,24 @@
 import { useState } from 'react';
 import { Icon } from '@/components';
 import { parseNumber } from '@/utils/numberUtil';
+import { COLORS } from '@/constants';
+import { Graph } from './Graph';
 
 interface IProp {
   views: number;
-  before_views: number;
   date: string;
   title: string;
-  total_views: number;
   likes: number;
   id: string;
 }
 
 export const Section = (p: IProp) => {
   const [open, setOpen] = useState(false);
-  const doesViewIncreased = p.views - p.before_views > 0;
 
   return (
-    <section className="flex flex-col w-full h-fit rounded-[4px]">
+    <section className="flex flex-col w-full h-fit rounded-[4px] relative">
       <div
-        className={`p-[25px] min-h-[87.5px] cursor-pointer bg-bg-sub max-xl:flex-col flex relative justify-between items-center gap-4 max-MBI:flex-col max-MBI:pb-[35px]`}
+        className={`p-[25px] min-h-[87.5px] cursor-pointer bg-bg-sub max-xl:flex-col flex  justify-between items-center gap-4 max-MBI:flex-col max-MBI:pb-[35px]`}
         onClick={() => setOpen((prev) => !prev)}
       >
         <span
@@ -33,17 +32,15 @@ export const Section = (p: IProp) => {
         <div className="flex items-center text-[20px] font-medium justify-between text-text-alt gap-1 max-TBL:text-[15px] max-MBI:w-full">
           <span className="MBI:hidden">{p.date.split('T')[0]}</span>
           <div className="flex items-center gap-[6px]">
-            <span className='after:content-["_/"]'>
-              {parseNumber(p.total_views)}
+            <span className='after:content-["/"] after:ml-2'>
+              {parseNumber(p.views * 100)}
             </span>
-            <span
-              className={`after:ml-1 ${doesViewIncreased ? 'after:text-primary-sub after:content-["↑"]' : 'after:text-destructive-sub after:content-["↓"]'}`}
-            >
+            <span className="flex items-center after:ml-2 before:text-primary-sub before:content-['↑'] before:mr-1 after:content-['/']">
               {parseNumber(p.views)}
             </span>
-            <span>/</span>
             <Icon
               name="Like"
+              color={COLORS.primary.sub}
               className="w-[23px] max-TBL:w-[19px] max-MBI:w-[16px]"
             />
             <span>{parseNumber(p.likes)}</span>
@@ -61,6 +58,7 @@ export const Section = (p: IProp) => {
           className="MBI:hidden absolute bottom-3"
         />
       </div>
+      {open && <Graph />}
     </section>
   );
 };
