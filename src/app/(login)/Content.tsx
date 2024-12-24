69f199a602114767213f9c64ff0eed445df9e14b
six-standard <dbrrl1127@gmail.com>
2024-12-25 00:16:19 +0900

    feature: 로그인 페이지 반응형 추가

--- src/app/(login)/Content.tsx
+++ src/app/(login)/Content.tsx
@@ -23,16 +23,16 @@ export const Content = () => {
   });
 
   return (
-    <main className="w-full h-full flex items-center justify-center">
+    <main className="w-full h-full flex justify-center MBI:items-center max-MBI:p-[40px_30px]">
       <form
         onSubmit={handleSubmit((data: LoginVo) => mutate(data))}
-        className="w-fit h-[480px] flex overflow-hidden bg-bg-sub rounded-[4px] shadow-[0_4px_16px_0_rgba(0,0,0,.04)]"
+        className="w-fit h-[480px] flex overflow-hidden bg-bg-sub rounded-[4px] shadow-[0_4px_16px_0_rgba(0,0,0,.04)] max-MBI:bg-bg-main max-MBI:h-fit max-MBI:w-full"
       >
-        <div className="w-[220px] h-full bg-bg-alt flex items-center justify-center">
+        <div className="w-[220px] h-full bg-bg-alt flex items-center justify-center max-MBI:hidden ">
           <Image width={96} height={96} src="/favicon.png" alt="로고 이미지" />
         </div>
-        <div className="w-[500px] h-full flex flex-col gap-[30px] items-center justify-center">
-          <h1 className="font-medium text-[32px] text-text-main">
+        <div className="w-[500px] h-full flex flex-col gap-[30px] items-center justify-center max-MBI:w-full">
+          <h1 className="font-medium text-[32px] text-text-main max-MBI:text-[22px] max-MBI:font-normal flex items-center gap-5 before:inline-block before:bg-[url('/favicon.png')] before:[background-size:_100%_100%] before:w-[64px] before:h-[64px]">
             Velog Dashboard
           </h1>
           <Input
@@ -40,6 +40,7 @@ export const Content = () => {
             size="LARGE"
             type="password"
             placeholder="Access Token을 입력하세요"
+            className="max-MBI:w-[100%_!important]"
             {...register('accessToken', { required: true })}
           />
           <Input
@@ -47,12 +48,14 @@ export const Content = () => {
             size="LARGE"
             type="password"
             placeholder="Refresh Token을 입력하세요"
+            className="max-MBI:w-[100%_!important]"
             {...register('refreshToken', { required: true })}
           />
           <Button
             size="LARGE"
             form="LARGE"
             type="submit"
+            className="max-MBI:w-[100%_!important]"
             disabled={!isValid}
             id="login"
           >
--- src/constants/sizes.constant.ts
+++ src/constants/sizes.constant.ts
@@ -1,4 +1,5 @@
 export const SIZES = {
+  FULL: 'w-[100%]',
   LARGE: 'w-[400px]',
   MEDIUM: 'w-[200px]',
   SMALL: 'w-[100px]',
