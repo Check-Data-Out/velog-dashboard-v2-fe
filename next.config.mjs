3e942644c5ac370ac0f18caa0ca9eba67d181775
six-standard <dbrrl1127@gmail.com>
2024-12-28 02:38:55 +0900

    modify: CORS 관련 오류 해결

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,10 @@ const nextConfig = {
     });
     return config;
   },
+
+  images: {
+    domains: ['velog.velcdn.com'],
+  },
 };
 export default withSentryConfig(nextConfig, {
   // For all available options, see:
