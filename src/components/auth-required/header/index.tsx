e99108540a14180234385fe0e59c061a38281dcc
six-standard <dbrrl1127@gmail.com>
2025-01-07 01:59:07 +0900

    refactor: 헤더 리팩토링

--- src/components/auth-required/header/index.tsx
+++ src/components/auth-required/header/index.tsx
@@ -3,31 +3,26 @@
 import { useEffect, useRef, useState } from 'react';
 import { usePathname, useRouter } from 'next/navigation';
 import Image from 'next/image';
-import Link from 'next/link';
 import { useMutation, useQuery } from '@tanstack/react-query';
-import { COLORS, PATHS, SCREENS } from '@/constants';
-import { Icon, NameType } from '@/components';
+import { PATHS, SCREENS } from '@/constants';
+import { NameType } from '@/components';
 import { useResponsive } from '@/hooks';
 import { logout, me } from '@/apis';
+import { defaultStyle, Section, textStyle } from './Section';
 
 const layouts: Array<{ icon: NameType; title: string; path: string }> = [
-  { icon: 'Analytics', title: '내 통계', path: '/main' },
-  { icon: 'LeaderBoards', title: '리더보드', path: '/leaderboards' },
+  { icon: 'Analytics', title: '내 통계', path: '/main?asc=false&sort=' },
+  { icon: 'LeaderBoards', title: '리더보드', path: '/leaderboards?type=views' },
   { icon: 'Compare', title: '통계 비교', path: '/compare' },
 ];
-
-const defaultStyle =
-  'w-[180px] h-[65px] px-9 transition-all duration-300 shrink-0 max-MBI:w-[65px] max-MBI:px-0 ';
-const navigateStyle = 'gap-5 flex items-center justify-center cursor-pointer ';
 
 export const Header = () => {
   const [open, setOpen] = useState(false);
   const menu = useRef<HTMLDivElement | null>(null);
+  const path = usePathname();
   const router = useRouter();
   const width = useResponsive();
-  const path = usePathname();
-  const textStyle = (currentPath: string) =>
-    `${currentPath === path ? 'text-TEXT-MAIN' : 'text-TEXT-ALT'} text-ST4 shrink-0 transition-all duration-300 max-MBI:hidden `;
+  const barWidth = width < SCREENS.MBI ? 65 : 180;
 
   const { mutate: out } = useMutation({
     mutationFn: logout,
@@ -52,32 +47,40 @@ export const Header = () => {
 
   return (
     <nav className="w-full max-MBI:flex max-MBI:justify-center">
-      <div className="flex w-fit relative">
-        <div
-          style={{
-            transform: `translateX(${layouts.findIndex((i) => i.path === path) * (width < SCREENS.MBI ? 65 : 180)}px)`,
-          }}
-          className={`${defaultStyle} h-[2px_!important] bg-TEXT-MAIN absolute bottom-0 left-0`}
-        />
-        {layouts.map((i) => (
-          <Link
-            href={i.path}
-            key={i.title}
-            className={defaultStyle + navigateStyle}
-          >
-            <Icon
-              size={25}
-              color={COLORS.TEXT[i.path === path ? 'MAIN' : 'ALT']}
-              name={i.icon}
-            />
-            <span className={textStyle(i.path)}>{i.title}</span>
-          </Link>
-        ))}
-        <div className="w-fit h-fit flex flex-col relative z-50" ref={menu}>
+      <div className="flex w-fit">
+        <Section clickType="none">
+          <Image
+            width={35}
+            height={35}
+            src={'/favicon.png'}
+            className="transition-all"
+            alt=""
+          />
+          <span className="text-TEXT-MAIN text-T4 max-TBL:hidden">
+            Velog Dashboard
+          </span>
+        </Section>
+        <div className="flex w-fit relative">
           <div
-            onClick={() => setOpen((prev) => !prev)}
-            className={`${defaultStyle + navigateStyle} transition-[none_!important] min-w-[180px] rounded-t-[4px] w-[fit-content_!important] max-MBI:min-w-[0] max-MBI:w-[65px_!important]`}
-          >
+            style={{
+              transform: `translateX(${layouts.findIndex((i) => i.path.split('?')[0] === path) * barWidth}px)`,
+            }}
+            className={`${defaultStyle} h-[2px_!important] bg-TEXT-MAIN absolute bottom-0 left-0`}
+          />
+          {layouts.map((i) => (
+            <Section
+              key={i.title}
+              clickType="link"
+              icon={i.icon}
+              action={i.path}
+            >
+              {i.title}
+            </Section>
+          ))}
+        </div>
+
+        <div className="w-fit h-fit flex flex-col relative z-50" ref={menu}>
+          <Section clickType="function" action={() => setOpen((prev) => !prev)}>
             <Image
               width={35}
               height={35}
@@ -85,10 +88,10 @@ export const Header = () => {
               src={profiles?.profile.thumbnail || '/profile.jpg'}
               alt=""
             />
-            <span className={textStyle('username')}>
+            <span className={textStyle + 'text-TEXT-ALT'}>
               {profiles?.username || 'NULL'}
             </span>
-          </div>
+          </Section>
           {open && (
             <div className="flex flex-col items-center max-MBI:items-end absolute self-center top-[50px] max-MBI:right-[6px]">
               <div className="w-0 h-0 border-[15px] ml-3 mr-3 border-TRANSPARENT border-b-BG-SUB" />
