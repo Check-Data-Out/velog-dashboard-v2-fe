605cf9b84b8cdd3b8703e2f255882dbaa5577fe2
six-standard <dbrrl1127@gmail.com>
2025-01-07 18:59:16 +0900

    modify: 이벤트 트래킹 개선

--- src/utils/trackUtil.tsx
+++ src/utils/trackUtil.tsx
@@ -8,11 +8,27 @@ type VisitDataType = {
   unloadDate?: string;
 };
 
-type MessageType = '01' | '02' | '03' | '04' | '99';
+enum MessageType {
+  '01-01' = 'ENTER',
+  '01-02' = 'LOGIN',
+  '01-03' = 'NAVIGATE',
+  '02-01' = 'SECTION_INTERACT',
+  '02-02' = 'SORT_INTERACT_MAIN',
+  '02-03' = 'REFRESH_INTERACT',
+  '03-01' = 'SORT_INTERACT_BOARD',
+}
+
+const EVENT_LOG = process.env.NEXT_PUBLIC_EVENT_LOG;
+
+if (EVENT_LOG === undefined) {
+  throw new Error('EVENT_LOG가 ENV에서 설정되지 않았습니다.');
+}
 
 export const trackUserEvent = (event_type: MessageType) => {
   const { pathname: path } = window.location;
-  instance('/event', { body: { path, event_type }, method: 'POST' });
+  if (EVENT_LOG === 'true') {
+    instance('/event', { body: { path, event_type }, method: 'POST' });
+  }
 };
 
 export const TrackVisitEvent = () => {
@@ -22,22 +38,19 @@ export const TrackVisitEvent = () => {
   });
 
   const setUnloadData = () => {
-    data.current = {
-      ...data.current,
-      unloadDate: new Date().toISOString(),
-    };
+    data.current = { ...data.current, unloadDate: new Date().toISOString() };
     instance('/stay', { body: data.current, method: 'POST', keepalive: true });
   };
 
   useEffect(() => {
     // 페이지 로드 시 시간 기록
     data.current.loadDate = new Date().toISOString();
-    if (process.env.NODE_ENV === 'production') {
+    if (process.env.NODE_ENV === 'production' && EVENT_LOG === 'true') {
       window.addEventListener('unload', setUnloadData);
     }
 
     return () => {
-      if (process.env.NODE_ENV === 'production') {
+      if (process.env.NODE_ENV === 'production' && EVENT_LOG === 'true') {
         window.removeEventListener('unload', setUnloadData);
       }
     };
