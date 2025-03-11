495434f7b43deb924f2d6381980a50fbc88c6bdd
six-standard <dbrrl1127@gmail.com>
2025-03-11 17:16:24 +0900

    refactor: 공지사항 반응형 지원

--- src/components/auth-required/notice/Modal.tsx
+++ src/components/auth-required/notice/Modal.tsx
@@ -11,22 +11,24 @@ export const Modal = () => {
   const { data } = useQuery({ queryKey: [PATHS.NOTIS], queryFn: notiList });
 
   return (
-    <div className="w-[800px] h-[500px] overflow-auto flex flex-col gap-5 p-10 rounded-md bg-BG-SUB">
+    <div className="w-[800px] h-[500px] max-MBI:w-[450px] max-MBI:h-[200px] overflow-auto flex flex-col gap-3 p-10 max-MBI:p-7 rounded-md bg-BG-SUB">
       <div className="flex items-center justify-between">
-        <h2 className="text-TEXT-MAIN items-cenetr gap-3 text-T3">공지사항</h2>
+        <h2 className="text-TEXT-MAIN items-cenetr gap-3 text-T3 max-MBI:text-T4">
+          공지사항
+        </h2>
         <Icon name="Close" onClick={close} className="cursor-pointer" />
       </div>
 
       {data?.posts?.map(({ content, created_at, id, title }) => (
-        <div key={id}>
+        <div key={id} className="flex flex-col gap-2">
           <div className="flex items-center gap-3">
-            <h3 className="text-TEXT-MAIN text-T4">{title}</h3>
-            <h4 className="text-TEXT-ALT text-T5">
+            <h3 className="text-TEXT-MAIN text-T4 max-MBI:text-T5">{title}</h3>
+            <h4 className="text-TEXT-ALT text-T5 max-MBI:text-ST5">
               {created_at.split('T')[0]}
             </h4>
           </div>
           <div
-            className="text-TEXT-MAIN text-ST5"
+            className="text-TEXT-MAIN text-I4"
             dangerouslySetInnerHTML={{ __html: content }}
           ></div>
         </div>
