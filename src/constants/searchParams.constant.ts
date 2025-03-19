186c1b48f5a5cdaf1094e3cbbcd620a646809a8d
six-standard <dbrrl1127@gmail.com>
2025-03-19 09:22:58 +0900

    modify: 오늘조회순 추가 오늘조회순보다 짧으면서 이해하기 쉬운 단어가 떠오르지 않네요

--- src/constants/searchParams.constant.ts
+++ src/constants/searchParams.constant.ts
@@ -1,5 +1,6 @@
 export const SORT_TYPE = {
   작성일순: '',
   조회순: 'dailyViewCount',
+  오늘조회순: 'todayViewCount',
   좋아요순: 'dailyLikeCount',
 } as const;
--- src/types/searchParams.type.ts
+++ src/types/searchParams.type.ts
@@ -1,6 +1,6 @@
 import { SORT_TYPE } from '@/constants';
 
-export type SortKey = '작성일순' | '조회순' | '좋아요순';
+export type SortKey = '작성일순' | '조회순' | '오늘조회순' | '좋아요순';
 export type SortValue = (typeof SORT_TYPE)[keyof typeof SORT_TYPE];
 
 export type SortType = Record<SortKey, string>;
