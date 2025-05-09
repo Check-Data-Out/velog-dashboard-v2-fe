0006a8fe14e28dc76006af16ea15d67352398f39
six-standard <dbrrl1127@gmail.com>
2025-05-09 15:56:05 +0900

    modify: 필요없는 파일 제거

--- src/app/qr-login/page.tsx
+++
@@ -1,8 +0,0 @@
-import { Metadata } from 'next';
-import { Content } from './Content';
-
-export const metadata: Metadata = { title: '로그인' };
-
-export default function Page() {
-  return <Content />;
-}
