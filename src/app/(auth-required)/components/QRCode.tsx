4b49e57b7d1cec93292232be885479765df3009f
six-standard <dbrrl1127@gmail.com>
2025-05-10 12:48:00 +0900

    refactor: 레이아웃 적용

--- src/app/(auth-required)/components/QRCode.tsx
+++ src/app/(auth-required)/components/QRCode.tsx
@@ -2,22 +2,18 @@
 
 import { QRCodeSVG } from 'qrcode.react';
 import { useQuery } from '@tanstack/react-query';
-import { Icon } from '@/components';
-import { useModal } from '@/hooks/useModal';
 import { COLORS, env, PATHS, SCREENS } from '@/constants';
 import { useResponsive } from '@/hooks';
 import { createQRToken } from '@/apis';
+import { Modal as Layout } from '@/components';
 
 export const QRCode = () => {
-  const { close } = useModal();
   const width = useResponsive();
 
   const { data, isSuccess } = useQuery({ queryKey: [PATHS.QRLOGIN], queryFn: createQRToken });
 
   return (
-    <div className="w-fit h-fit overflow-auto flex flex-col items-end gap-5 p-10 max-MBI:p-7 rounded-md bg-BG-SUB">
-      <Icon name="Close" onClick={close} color="#FFFFFF" className="cursor-pointer" />
-
+    <Layout title="QR 로그인" className="w-fit h-fit">
       {isSuccess ? (
         <QRCodeSVG
           value={`${env.BASE_URL}/api/qr-login?token=${data.token}`}
@@ -35,6 +31,6 @@ export const QRCode = () => {
       <span className="text-TEXT-MAIN text-ST4 transition-all max-MBI:text-ST5 w-full text-center">
         해당 QR을 찍어 로그인
       </span>
-    </div>
+    </Layout>
   );
 };
