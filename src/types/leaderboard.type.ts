05c7f0aee8c94a4ab9aeedc4b2a8b58efc7ea93b
six-standard <dbrrl1127@gmail.com>
2025-05-15 19:22:31 +0900

    refactor: 타입 분리

--- src/types/leaderboard.type.ts
+++ src/types/leaderboard.type.ts
@@ -1,25 +1,28 @@
-export type LeaderboardListDto = {
-  users?: Array<{
-    id: string;
-    email: string;
-    totalViews: string;
-    totalLikes: string;
-    totalPosts: string;
-    viewDiff: string;
-    likeDiff: string;
-    postDiff: string;
-  }>;
-  posts?: Array<{
-    id: string;
-    title: string;
-    slug: string;
-    totalViews: number;
-    totalLikes: number;
-    viewDiff: number;
-    likeDiff: number;
-    releasedAt: string;
-  }>;
+type LeaderboardListUser = {
+  id: string;
+  email: string;
+  totalViews: string;
+  totalLikes: string;
+  totalPosts: string;
+  viewDiff: string;
+  likeDiff: string;
+  postDiff: string;
+};
+type LeaderboardListPost = {
+  id: string;
+  title: string;
+  slug: string;
+  totalViews: number;
+  totalLikes: number;
+  viewDiff: number;
+  likeDiff: number;
+  releasedAt: string;
 };
 
-export type LeaderboardItemType = NonNullable<LeaderboardListDto['users']>[number] &
-  NonNullable<LeaderboardListDto['posts']>[number];
+export type LeaderboardListDto = {
+  users?: LeaderboardListUser[];
+  posts?: LeaderboardListPost[];
+};
+
+export type LeaderboardItemType = NonNullable<LeaderboardListUser> &
+  NonNullable<LeaderboardListPost>;
