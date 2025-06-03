42fe2376cc60d1084bb7a8f2410bd3ec4ee6d1fc
six-standard <dbrrl1127@gmail.com>
2025-06-03 15:36:10 +0900

    refactor: id 값 변경 추가로 필요없는 id prop 제거

--- src/app/(auth-required)/main/components/Summary/BarContent.tsx
+++ src/app/(auth-required)/main/components/Summary/BarContent.tsx
@@ -5,20 +5,18 @@ interface IProp {
   content: number;
   increasement?: number;
   prefix?: string;
-  id?: string;
 }
 
 const afterContent =
   'before:text-PRIMARY-SUB before:content-[attr(data-increasement)_"↑"] before:mr-2 before:text-T5';
 
-export const BarContent = ({ title, content, increasement, prefix = '개', id }: IProp) => {
+export const BarContent = ({ title, content, increasement, prefix = '개' }: IProp) => {
   return (
     <div className="w-full flex justify-between items-center">
       <span className="text-ST5 text-TEXT-ALT">{title}</span>
       <span
         className={`flex items-center text-TEXT-MAIN text-T4 ${increasement ? afterContent : ''}`}
         data-increasement={parseNumber(increasement ? increasement : 0)}
-        id={id}
       >
         {parseNumber(content) + prefix}
       </span>
