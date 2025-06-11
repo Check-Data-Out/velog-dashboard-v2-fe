6527b0241a9618743bb5a760b57b72ffc160bc41
six-standard <dbrrl1127@gmail.com>
2025-06-11 09:57:12 +0900

    refactor: 부정적인 어투의 메세지 제거

--- src/app/TermsOfService.tsx
+++ src/app/TermsOfService.tsx
@@ -11,10 +11,6 @@ export const TermsOfService = () => {
       <a href={PRIVACY_POLICY_URL} className="text-TEXT-ALT text-[14px]">
         개인정보처리방침
       </a>
-      <div className="w-[1px] h-[15px] bg-TEXT-ALT max-MBI:hidden" />
-      <span className="text-TEXT-ALT text-[14px] max-MBI:hidden">
-        해당 서비스를 사용하실 경우, 약관에 동의하는 것으로 간주합니다.
-      </span>
     </div>
   );
 };
