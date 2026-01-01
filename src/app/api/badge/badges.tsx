b52b36bd0685077421f3a9956221be29cc499d69
six-standard <office@stdd.dev>
2026-01-01 17:42:32 +0900

    fix: import 오류 해결

--- src/app/api/badge/badges.tsx
+++ src/app/api/badge/badges.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/no-unknown-property */
 
-import { BadgeGenerator } from '@/app/components/BadgeGenerator';
 import { BadgeDto } from '@/types';
 import { Posts, PoweredBy, Statistics, Title } from './components';
 import { createImageResponse } from './util';
