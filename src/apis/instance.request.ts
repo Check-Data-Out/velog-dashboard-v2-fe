297dd4353ab673b6c524e7ce39ef0f960af3f782
six-standard <github@stdd.dev>
2026-01-13 23:45:15 +0900

    refactor: 센트리 코드 개선

--- src/apis/instance.request.ts
+++ src/apis/instance.request.ts
@@ -1,4 +1,4 @@
-import { captureException, setContext } from '@sentry/nextjs';
+import { captureException, withScope } from '@sentry/nextjs';
 import returnFetch, { FetchArgs } from 'return-fetch';
 import { ENVS } from '@/constants';
 import { ServerNotRespondingError } from '@/errors';
@@ -50,7 +50,7 @@ const fetch = returnFetch({
 export const instance = async <I, R>(
   input: URL | RequestInfo,
   init?: InitType<I>,
-  error?: Record<string, CustomError>,
+  errorTypes?: Record<string, CustomError>,
 ): Promise<R> => {
   let cookieHeader = '';
   if (typeof window === 'undefined') {
@@ -76,33 +76,40 @@ export const instance = async <I, R>(
 
     return (data.body as unknown as SuccessType<R>).data;
   } catch (err: unknown) {
-    const context = err as Response;
-    if (!context.ok && context.status === 401) {
+    const errAsResponse = err as Response;
+    const errAsError = err as Error;
+    const customError = errorTypes?.[errAsResponse.status];
+    let response: unknown;
+
+    if (!errAsResponse.ok && errAsResponse.status === 401) {
       if (location) window.location.replace('/');
       return {} as never;
     }
 
-    setContext('Request', {
-      path: context.url,
-      status: context.status,
+    withScope((scope) => {
+      scope.setContext('Request', {
+        url: '/api' + input,
+        method: init?.method,
+        body: init?.body,
+      });
+      scope.setContext('Detailed Information', {
+        name: errAsError.name,
+        message: errAsError.message,
+        cause: errAsError.cause,
+        status: errAsResponse.status,
+      });
+
+      if (errAsError.name === 'TimeoutError') {
+        // Timeout 오류는 별도의 status값이 없는 것으로 보여 이런 형태로 처리합니다
+        response = new ServerNotRespondingError();
+      } else if (!customError) {
+        response = new Error(`서버에서 예기치 않은 오류가 발생했습니다. (${errAsError.name})`);
+      } else if (customError.shouldCaptureException) {
+        response = customError;
+      }
+      captureException(response);
     });
-    if ((err as Error).name === 'TimeoutError') {
-      captureException(new ServerNotRespondingError());
-      throw new ServerNotRespondingError();
-    } else {
-      if (!error?.[`${(err as Response).status}`]) {
-        const serverError = new Error(
-          `서버에서 예기치 않은 오류가 발생했습니다. (${(err as Error).name})`,
-        );
-        captureException(serverError);
-        throw serverError;
-      }
 
-      if (error[`${(err as Response).status}`].shouldCaptureException) {
-        captureException(error[`${(err as Response).status}`]);
-      }
-
-      throw error[`${(err as Response).status}`];
-    }
+    throw response;
   }
 };
